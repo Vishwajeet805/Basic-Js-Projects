adee9f9bf7fc136dafbc59a137bdadd7774b2f6b
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2026-02-04 10:20:00 +0530

    Counter project completed

--- Counter/index.js
+++ Counter/index.js
@@ -4,6 +4,7 @@ const increaseBtn = document.getElementById("Increase");
 const countLabel = document.getElementById("countLabel");
 let count = 0;
 increaseBtn.onclick = function () {
+    
     count++;
     countLabel.textContent = count;
 }
